fafd4796dfca83aacd8e7f6eec6ebf13f4187394
alekya <alekyakuncharam@gmail.com>
2025-01-21 11:31:47 +0530

    updated

---
+++ src/day10.java
@@ -0,0 +1,44 @@
+public class day10 {
+    public static void main(String[] args){
+
+    }
+
+    public static void mergeArrays(int[] nums1,int m,int[] nums2,int n) {
+        //int arr[]=new int[m+n];
+
+        int p1=0;
+        int p2=0;
+        //int p3=0;
+        
+
+        while(p1<m && p2<n) {
+            if(nums1[p1] <nums2[p2]){
+                //arr[p3]=nums1[p1];
+                p1++;
+               // p3++;
+                
+            } else{
+               // arr[p3]==nums2[p2];
+                //p2++;
+               // p3++;
+                int temp=nums1[p1];
+                nums[p1]==nums2[p2];
+                nums2[p2]=temp;
+            }
+
+        }
+        while(p1<m){
+            //arr[p3]==nums1[p1];
+            nums[p1]=nums[p2];
+            //p3++
+        }
+        while(p2>m){
+            //arr[p3]==nums2[p2];
+            nums[p1]=nums[p2];
+
+            p2++;
+            p1++;
+        }
+        
+    }
+}
